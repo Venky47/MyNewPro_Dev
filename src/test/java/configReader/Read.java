ed2c83d6b715cab50a52497ce13fb616a9850825
venkatesh <venky47.sri@gmail.com>
2022-08-17 13:04:03 +0530

    3rd

--- src/test/java/configReader/Read.java
+++ src/test/java/configReader/Read.java
@@ -3,5 +3,6 @@ package configReader;
 public class Read {
     public static void main(String[] args) {
         System.out.println("testing git");
+        System.out.println("testing git-2");
     }
 }
